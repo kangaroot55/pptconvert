0a469a1a30fc3b54ff0c82c21f39abf7d9544930
Song Jisan <kangaroot55@gmail.com>
2013-05-02 19:20:15 +0900

    0502

--- src/test.scala
+++ src/test.scala
@@ -1,29 +1,3 @@
-/*
-FileInputStream is = new FileInputStream("slideshow.ppt");
-SlideShow ppt = new SlideShow(is);
-is.close();
-
-Dimension pgsize = ppt.getPageSize();
-
-Slide[] slide = ppt.getSlides();
-for (int i = 0; i < slide.length; i++) {
-
-    BufferedImage img = new BufferedImage(pgsize.width, pgsize.height, BufferedImage.TYPE_INT_RGB);
-    Graphics2D graphics = img.createGraphics();
-    //clear the drawing area
-    graphics.setPaint(Color.white);
-    graphics.fill(new Rectangle2D.Float(0, 0, pgsize.width, pgsize.height));
-
-    //render
-    slide[i].draw(graphics);
-
-    //save the output
-    FileOutputStream out = new FileOutputStream("slide-"  + (i+1) + ".png");
-    javax.imageio.ImageIO.write(img, "png", out);
-    out.close();
-}
-*/
-
 
 
 
@@ -41,94 +15,136 @@ import java.io.FileInputStream
 import java.awt.geom.Rectangle2D
 import org.apache.poi.hslf.usermodel.SlideShow
 import org.apache.poi.hslf.model.Slide
+import org.apache.poi.xslf.extractor.XSLFPowerPointExtractor
+import org.apache.poi.xslf.XSLFSlideShow
+import org.apache.poi.hslf.extractor.PowerPointExtractor
+import org.apache.poi.hslf.HSLFSlideShow
 //package org.apache.poi.xslf.util
 
 
 
 
 object pptx2png {
-  
-	def main(args: Array[String]) {
-	  
-	    val address = "c:\\jot\\3d.pptx"
-	    
-//	    val add = address.TypeDetector()
-		
-		val pos = address.lastIndexOf(".")
-		val ext = address.substring( pos + 1 )
-	
-		ext match{
-		  case "pptx" => pptx(address)
-		  case "ppt" => ppt(address)
-		}
-		
-		def pptx(args: String){
+
+  		def pptx(args: String, saveFolder: String) : Array[String] = {
+		  
 			var is = new FileInputStream(args)
-		
+
+			
 			var ppt = new XMLSlideShow(is)
+			
+			var extract = new XSLFPowerPointExtractor(ppt)
+			
+//			Metadata extract
+//			System.out.println(extract.getText())
+//			System.out.println(extract.getMetadataTextExtractor().getText())
+			
 			is.close()
 			
 			val pgsize = ppt.getPageSize()
 			val slide = ppt.getSlides()
 			
-			def myFunc(s: XSLFSlide, i: Int){
+			def myFunc(s: XSLFSlide, i: Int) : String = {
 				val img = new BufferedImage(pgsize.width, pgsize.height, BufferedImage.TYPE_INT_RGB)
 				val graphics = img.createGraphics()
 				
-				graphics.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-		        graphics.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
-		        graphics.setRenderingHint(RenderingHints.KEY_INTERPOLATION,RenderingHints.VALUE_INTERPOLATION_BICUBIC);
-		        graphics.setRenderingHint(RenderingHints.KEY_FRACTIONALMETRICS,RenderingHints.VALUE_FRACTIONALMETRICS_ON);
+				
+				graphics.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON)
+		        graphics.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY)
+		        graphics.setRenderingHint(RenderingHints.KEY_INTERPOLATION,RenderingHints.VALUE_INTERPOLATION_BICUBIC)
+				graphics.setRenderingHint(RenderingHints.KEY_FRACTIONALMETRICS,RenderingHints.VALUE_FRACTIONALMETRICS_ON)
 
 		        
 				img.createGraphics().setPaint(Color.white)
 				img.createGraphics().fill(new Rectangle2D.Float(0, 0, pgsize.width, pgsize.height))
 				
-				
 				s.draw(graphics)
 				
-				var out = new FileOutputStream("c:\\jot\\pptx-slide-"+i+".png")
+				var out = new FileOutputStream(saveFolder+i+".png")
+				
 				javax.imageio.ImageIO.write(img, "png", out)
 				out.close()
+				
+				return (saveFolder+i+".png")
 			}
 			
 			
 			val a = slide.zipWithIndex.map{ case(s, i) => myFunc(s, i)}
+			
+			return a
+			
 		}
 		
-		def ppt(args: String){
+		
+		def ppt(args: String, saveFolder: String) : Array[String] = {
+		  
 			var is = new FileInputStream(args)
 			
-			var ppt = new SlideShow(is)
+			var pptex = new HSLFSlideShow(is) 
+			var ppt = new SlideShow(pptex)
+			
+			var extract = new PowerPointExtractor(pptex)
+			
+//			Metadata extract
+//			System.out.println(extract.getText())
+//			System.out.println(extract.getMetadataTextExtractor().getText())
+			
 			is.close()
 			
 			val pgsize = ppt.getPageSize()
 			val slide = ppt.getSlides()
 			
+			println(pgsize)
 
-			def myFunc(s: Slide, i: Int){
-				val img = new BufferedImage(pgsize.width, pgsize.height, BufferedImage.TYPE_INT_RGB)
+			def myFunc(s: Slide, i: Int) : String = {
+			  
+				val img = new BufferedImage(pgsize.width*2, pgsize.height*2, BufferedImage.TYPE_INT_RGB)
 				val graphics = img.createGraphics()
 
-				graphics.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-		        graphics.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
-		        graphics.setRenderingHint(RenderingHints.KEY_INTERPOLATION,RenderingHints.VALUE_INTERPOLATION_BICUBIC);
-		        graphics.setRenderingHint(RenderingHints.KEY_FRACTIONALMETRICS,RenderingHints.VALUE_FRACTIONALMETRICS_ON);
+				graphics.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON)
+				graphics.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY)
+				graphics.setRenderingHint(RenderingHints.KEY_INTERPOLATION,RenderingHints.VALUE_INTERPOLATION_BICUBIC)
+				graphics.setRenderingHint(RenderingHints.KEY_FRACTIONALMETRICS,RenderingHints.VALUE_FRACTIONALMETRICS_ON)
 
-
-				img.createGraphics().setPaint(Color.white)
-				img.createGraphics().fill(new Rectangle2D.Float(0, 0, pgsize.width, pgsize.height))
 				
+				graphics.scale(2,2)
+				img.createGraphics().setPaint(Color.white)
+				img.createGraphics().fill(new Rectangle2D.Float(0, 0, pgsize.width * 2, pgsize.height * 2)) 
+				
+			
 				
 				s.draw(graphics)
 				
-				var out = new FileOutputStream("c:\\jot\\ppt-slide-"+i+".png")
+				var out = new FileOutputStream(saveFolder+i+".png")
+			
 				javax.imageio.ImageIO.write(img, "png", out)
 				out.close()
+		
+				return (saveFolder+i+".png")
 			}
 			
 			val a = slide.zipWithIndex.map{ case(s, i) => myFunc(s, i)}
+			
+			return a
 		}
+
+		
+	def main(args: Array[String]) {
+	  
+	    val address = args(0)
+	    val saveFolder = args(1)
+	    
+
+		val pos = address.lastIndexOf(".")
+		val ext = address.substring( pos + 1 )
+	
+		
+		ext match{
+		  case "pptx" => pptx(address, saveFolder)
+		  case "ppt" => ppt(address, saveFolder)
+		}
+		
+		
 		
 		
 	}
